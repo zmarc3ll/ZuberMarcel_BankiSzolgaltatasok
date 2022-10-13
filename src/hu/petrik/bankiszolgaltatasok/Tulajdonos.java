69e391b6c40f43e0568354b74ab334fcaa1f859f
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-10-13 09:10:55 +0200

    tulajdonos class megírása

--- src/hu/petrik/bankiszolgaltatasok/Tulajdonos.java
+++ src/hu/petrik/bankiszolgaltatasok/Tulajdonos.java
@@ -2,4 +2,17 @@ package hu.petrik.bankiszolgaltatasok;
 
 public class Tulajdonos {
 
+    private String nev;
+
+    public Tulajdonos(String nev) {
+        this.nev = nev;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
 }
