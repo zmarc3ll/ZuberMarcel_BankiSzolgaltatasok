be7f8c4c829f5130d2b72f55c4ff0d681a61f8a0
zmarc3ll <zubermarcellpetrik@gmail.com>
2022-10-13 09:21:22 +0200

    BankiSzolgaltatas class megírása

--- src/hu/petrik/bankiszolgaltatasok/BankiSzolgaltatas.java
+++ src/hu/petrik/bankiszolgaltatasok/BankiSzolgaltatas.java
@@ -1,5 +1,13 @@
 package hu.petrik.bankiszolgaltatasok;
 
-public class BankiSzolgaltatas {
+public class BankiSzolgaltatas{
+    private Tulajdonos tulajdonos;
 
+    public BankiSzolgaltatas(Tulajdonos tulajdonos) {
+        this.tulajdonos = tulajdonos;
+    }
+
+    public Tulajdonos getTulajdonos() {
+        return tulajdonos;
+    }
 }
